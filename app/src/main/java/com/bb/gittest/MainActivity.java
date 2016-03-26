49541e060f1bfc3bfee047b392e692be672d4f8e
高强 <qiang.gao@24tidy.com>
2016-03-26 14:23:26 +0800

    界面添加点击事件

--- app/src/main/java/com/bb/gittest/MainActivity.java
+++ app/src/main/java/com/bb/gittest/MainActivity.java
@@ -8,8 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+
+        textView = (TextView) findViewById(R.id.tv_test);
+        textView.setOnClickListener(this);
     }
 
     @Override
@@ -47,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == textView) {
+            Toast.makeText(this, "这是我的一个测试", Toast.LENGTH_SHORT).show();
+        }
     }
 }
